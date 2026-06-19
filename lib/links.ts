6382d9f4b6b807d842094c194d7445fd9c506f14
khushikanwarin-ship-it <khushikanwar.in@gmail.com>
2026-06-19 15:02:02 +0530

    Update realtalkForm to use iframe for embedding

--- lib/links.ts
+++ lib/links.ts
@@ -14,10 +14,9 @@ export const SOCIAL = {
   // Google Form EMBED url for the Realtalk check.
   // Get it from your form: Send → < > (embed) → copy the src="..." URL.
   // It looks like: https://docs.google.com/forms/d/e/XXXX/viewform?embedded=true
-  realtalkForm: "https://docs.google.com/forms/d/e/1FAIpQLSfz01yhTq6Y2SELhzJ6wYxgPGk0R_-E7oYn6cXp-xMLM6V4fw/viewform?embedded=true",
+  realtalkForm: <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfz01yhTq6Y2SELhzJ6wYxgPGk0R_-E7oYn6cXp-xMLM6V4fw/viewform?embedded=true" width="640" height="2072" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>",
   // Direct (non-embedded) link to open the Realtalk Google Form in a new tab
-  realtalkFormPublic: "https://docs.google.com/forms/d/e/1FAIpQLSfz01yhTq6Y2SELhzJ6wYxgPGk0R_-E7oYn6cXp-xMLM6V4fw/viewform",
-};
+  realtalkFormPublic: "https://docs.google.com/forms/d/e/1FAIpQLSfz01yhTq6Y2SELhzJ6wYxgPGk0R_-E7oYn6cXp-xMLM6V4fw/viewform?usp=dialog",
 
 // Helper: returns a usable href, or "#" if not set yet
 export const href = (url: string) => url || "#";
